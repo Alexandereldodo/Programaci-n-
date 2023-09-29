e97c0be106321a06dd5d6720a3de61742f3b6500
Alexandereldodo <144471890+Alexandereldodo@users.noreply.github.com>
2023-09-28 22:23:50 -0400

    Create main.c

---
+++ programas explicados/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+/*
+void main(void)
+{
+    /*Se declaran dos variables de tipo float llamadas pag y spa. pag
+
+    float pag, spa;
+
+    /*Se inicializa la variable spa con el valor 0.
+    Esto se hace para asegurarse de que spa comience desde cero antes de sumar cualquier pago.
+
+
+    spa = 0;
+    printf("ingrese el primer pago: \t");
+
+   /* Se utiliza scanf para leer el valor que el usuario ingresa desde el teclado. %f indica que se espera un número decimal
+    scanf("%f", &pag);
+    while (pag)
+    {
+        spa = spa + pag;
+        printf("ingrese el siguiente pago: \t");
+        scanf("%f", &pag);
+    }
+    printf("\nel total de pagos del mes es: %.2f", spa);
+
+}
+*/
+
+/*
+#include <math.h>
+
+/*Esta línea incluye la biblioteca <math.h>, que proporciona funciones matemáticas
+
+void main(void)
+{
+    int num;
+    long cua, suc = 0;
+
+    /*Se declaran tres variables: num de tipo int para almacenar el número ingresado por el usuario, cua de tipo
+     long para almacenar el cuadrado del número
+
+
+    printf("\ningrese ingrese un numero entero -0 para determinar -:\t");
+    scanf("%d", &num);
+    while (num)
+
+
+       /*Se inicia un bucle while que se ejecutará mientras el valor de num sea diferente de cero.
+    {
+
+        cua = pow (num, 2);
+        printf("%d al cubo es %id", num, cua);
+        suc = suc + cua;
+        printf("\ningrese un numero entero -0 para terminar -:\t");
+        scanf("%d", &num);
+
+    }
+    printf("\nla suma de los cuadradros es %id", suc);
+
+
+}
+*/
+
+/*
+void main(void)
+{
+    int i;
+    float sal, nom;
+
+    /*Se declaran tres variables: i de tipo int para contar el número de profesores, sal de tipo
+    float para almacenar el salario de cada profesor, y nom de tipo float para llevar un seguimiento del total de la nómina.
+
+    nom = 0;
+
+    /*Se inicializa la variable nom con el valor 0. Esto se hace para asegurarse de que nom comience desde cero
+
+    for (i=1; i<=15; i++)
+
+        /*Se inicia un bucle for que se ejecutará 15 veces. El bucle se utiliza para ingresar los salarios de 15 profesores.
+    {
+        printf("\ningrese el salario del profesor%d: \t", i);
+        scanf("%f", &sal);
+        nom = nom + sal;
+
+    }
+    printf("\nel total de la nomina es: %.2f", nom);
+
+}
+*/
