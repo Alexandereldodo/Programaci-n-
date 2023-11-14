c9c3c17ee74e4be0d919cc3b705634e823a1ad83
Alexandereldodo <144471890+Alexandereldodo@users.noreply.github.com>
2023-11-14 03:09:49 -0400

    Loteria.c
    Menú, impresión, tirada

--- Loteria.c
+++ Loteria.c
@@ -4,15 +4,16 @@
 
 struct Registro /*crear una estructura registro de dos enteros
                 numerosaleatorios y variable de tiempo*/
-
 {
     int numerosAleatorios[3];
     int ano, mes, dia, hora, minuto, segundo;
+    
 };
 
 void imprimirNumerosConTiempo(struct Registro *registros, int numTiradas);
-void tirar(struct Registro *registros, int *numTiradas);
+void tirar(struct Registro **registros, int *numTiradas);
 /*funciones utilizadas en el programa que apuntan a la estuctura registro*/
+
 
 int main()
 {
@@ -23,32 +24,30 @@ int main()
 
     int opcion;
 
-    do  /*Inicia un bucle do que presenta un menú de opciones
+    do /*Inicia un bucle do que presenta un menú de opciones
            para seleccionar.*/
-
     {
         printf("\n0. Salir \n");
         printf("1. Realizar tirada \n");
         printf("2. Imprimir tiradas\n");
-        printf("\nElige una opcion: ");
+        
         scanf("%d", &opcion);
 
         switch (opcion)
         {
         case 0:
-            printf("\nHas elegido salir. Adios!\n");
+            printf("\nHas elegido salir. Adiós!\n");
             break;
         case 1:
             printf("\nHas elegido realizar una tirada.\n");
-            tirar(registros, &numTiradas);
+            tirar(&registros, &numTiradas);
             break;
         case 2:
             printf("\nHas elegido imprimir tiradas.\n\n");
             imprimirNumerosConTiempo(registros, numTiradas);
             break;
-
         default:
-            printf("\nOpcion no valida. Por favor, elige una opcion del 1 al 3.\n");
+            printf("\nOpción no válida. Por favor, elige una opción del 1 al 3.\n");
             break;
         }
 
@@ -70,7 +69,7 @@ void imprimirNumerosConTiempo(struct Registro *registros, int numTiradas)
     }
 }
 
-void tirar(struct Registro *registros, int *numTiradas)
+void tirar(struct Registro **registros, int *numTiradas)
 {
     time_t tiempoActual;
     time(&tiempoActual);
@@ -79,19 +78,20 @@ void tirar(struct Registro *registros, int *numTiradas)
 
     (*numTiradas)++;
 
-    registros = realloc(registros, sizeof(struct Registro) * (*numTiradas));
+    *registros = realloc(*registros, sizeof(struct Registro) * (*numTiradas));
 
     for (int i = 0; i < 3; i++)
     {
-        registros[*numTiradas - 1].numerosAleatorios[i] = rand() % 100;
+        (*registros)[*numTiradas - 1].numerosAleatorios[i] = rand() % 101;
     }
 
-    registros[*numTiradas - 1].ano = infoTiempo->tm_year + 1900;
-    registros[*numTiradas - 1].mes = infoTiempo->tm_mon + 1;
-    registros[*numTiradas - 1].dia = infoTiempo->tm_mday;
-    registros[*numTiradas - 1].hora = infoTiempo->tm_hour;
-    registros[*numTiradas - 1].minuto = infoTiempo->tm_min;
-    registros[*numTiradas - 1].segundo = infoTiempo->tm_sec;
+    (*registros)[*numTiradas - 1].ano = infoTiempo->tm_year + 1900;
+    (*registros)[*numTiradas - 1].mes = infoTiempo->tm_mon + 1;
+    (*registros)[*numTiradas - 1].dia = infoTiempo->tm_mday;
+    (*registros)[*numTiradas - 1].hora = infoTiempo->tm_hour;
+    (*registros)[*numTiradas - 1].minuto = infoTiempo->tm_min;
+    (*registros)[*numTiradas - 1].segundo = infoTiempo->tm_sec;
 
     printf("Tirada realizada y almacenada.\n");
 }
+
